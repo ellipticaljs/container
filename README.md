b51c2258c0ddb3a26b9acc4f410af75153feabd0
Scott <sfrancis@misinteractive.com>
2015-11-27 14:10:11 -0500

    Pushed Deprecation Notice

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 An es6 Map dependency injection container
-===========================
+=========================================
+
+# NOTICE
+
+This repo is now deprecated. The project has moved to https://github.com/ellipticaljs/container
--- dist/elliptical.container.js
+++ dist/elliptical.container.js
@@ -4,6 +4,10 @@
  * An es6 Map dependency injection container
  * =============================================================
  *
+ *  registerType<name,type>
+ *  getType<name>,
+ *  mapType<type,providerType>
+ *
  */
 
 //umd pattern
@@ -22,18 +26,15 @@
         root.returnExports = root.elliptical.Container;
     }
 }(this, function () {
-    var GET_TYPE_MAX_COUNT=10;
-    var GET_TYPE_INTERVAL=200;
-    var MAP_TYPE_MAX_COUNT=6;
-    var MAP_TYPE_INTERVAL=400;
+    var GET_TYPE_MAX_COUNT=6;
+    var GET_TYPE_INTERVAL=250;
 
     return {
         _registrations:{
             _container:null,
             get container() {
-                if (this._container) {
-                    return this._container;
-                } else {
+                if (this._container) return this._container;
+                else {
                     this._container=new Map();
                     return this._container;
                 }
@@ -52,7 +53,7 @@
 
         /**
          * registers a type
-         * @param {string} name 
+         * @param {string} name
          * @param {object} type
          */
         registerType:function(name,type){
@@ -70,12 +71,9 @@
             if(useDefaultTypeIfNotFound===undefined) useDefaultTypeIfNotFound=true;
             var container=this._registrations.container;
             var type=container.get(name);
-            if(type!==undefined){
-                return type;
-            }
-            if (!useDefaultTypeIfNotFound) {
-                return null;
-            } else {
+            if(type!==undefined) return type;
+            if (!useDefaultTypeIfNotFound) return null;
+            else {
                 type = container.get(this.defaultTypeName);
                 if (type !== undefined) {
                     try {
@@ -85,21 +83,25 @@
                     }
                     type[this.classNameProp] = name;
                     return type
-                } else {
-                    return null;
-                }
+                } else return null;
             }
         },
 
         /**
          * asynchronously returns the registered type associated with the name
          * @param {string} name - type name
+         * @param {boolean} useDefaultTypeIfNotFound
          * @param {function} callback - callback
          */
-        getTypeAsync:function(name,callback){
-            if(callback===undefined){
-                throw 'Error: getTypeAsync requires a callback function';
+        getTypeAsync:function(name,useDefaultTypeIfNotFound,callback){
+            var length = arguments.length;
+            ///support 2-3 params
+            if(length < 2) throw 'getTypeAsync expects a minimum of two parameters';
+            if(length===2){
+                callback=useDefaultTypeIfNotFound;
+                useDefaultTypeIfNotFound=true;
             }
+            if(typeof callback !=='function')throw 'Error: getTypeAsync requires a callback function';
             var self=this;
             var count=0;
             var type=this.getType(name,false);
@@ -107,9 +109,10 @@
                 callback(type);
             }else{
                 var timeoutId=setInterval(function(){
-                    //on the last attempt, we set useDefaultTypeIfNotFound to true
-                    var useDefaultTypeIfNotFound=(count ===(GET_TYPE_MAX_COUNT-1));
-                    type=self.getType(name,useDefaultTypeIfNotFound);
+                    //on the last attempt, we set useDefaultTypeIfNotFound to true, unless the parameter was originally passed in as false
+                    var _useDefaultTypeIfNotFound=(count ===(GET_TYPE_MAX_COUNT-1));
+                    if(!useDefaultTypeIfNotFound) _useDefaultTypeIfNotFound=false;
+                    type=self.getType(name,_useDefaultTypeIfNotFound);
                     if(type){
                         clearInterval(timeoutId);
                         callback(type);
@@ -123,68 +126,65 @@
         },
 
         /**
-         *  maps a registered provider to a type by the string name of the provider type
-         *  or: directly maps a provider type to a type
-         * @param {string} [name] - optional, string name of type
+         * mapType(<type>,<providerType>)
+         *   performs a setter dependency injection on a type with a providerType
+         *
+         *  (i)   registers a type and maps an unregistered providerType to that type
+         *
+         *  (ii)  registers a type and maps an previously registered providerType to that type by
+         *        name of the previously registered providerType
+         *
+         *  (iii) maps an unregistered providerType to a previously registered type by that type's name
+         *
+         *  mapType(typeName,Type,$Type)
+         *  mapType(typeName,Type,$typeName)
+         *  mapType(typeName,$Type)
+         *
+         * @param {string} name - string name of type
          * @param {object} type - the type
-         * @param {*} $name - string name of a registered provider type, or a provider type
+         * @param {*} $type - string name of a registered provider type, or a provider type
          */
-        mapType:function(name,type,$name){
+        mapType:function(name,type,$type){
+            var self=this;
             var container=this._registrations.container;
-            var classNameProp=this.classNameProp;
             var providerProp=this.providerProp;
-            var count=0;
-
-            if($name===undefined){
-                $name=type;
-                type=name;
-                name=null;
-            }
-
-            ///**** internal ********************************************************
-            // internal function that intermediates the correct parameter call to internal _mapType
-            var mapType_=function(t,$n,n){
-                var $providerType=self.getType($n);
-                if($providerType){
-                    (n) ? _mapType(n,t,$providerType) : _mapType(t,$providerType);
-                    return $providerType;
+            var length = arguments.length;
+            ///support 2-3 params
+            if(length < 2) throw 'mapType expects a minimum of two parameters';
+            if(length===2){
+                if(typeof name!=='string') throw "mapType with 2 params requires the first parameter to be the string name of a registered type";
+                $type=type;
+                type=this.getType(name,false);
+                if(type){
+                    type[providerProp]=$type;
+                    this.registerType(name,type);
                 }else{
-                    return null;
+                    this.getTypeAsync(name,false,function(dataType){
+                        if(dataType){
+                            dataType[providerProp]=$type;
+                            self.registerType(name,dataType);
+                        } else console.log('warning: container.mapType could not locate type: ' + name);
+                    });
                 }
-            };
-
-            //internal function that sets the mapping in the container
-            var _mapType=function(n,t,p){
-                //support 2-3 args
-                if(typeof n !=='string'){
-                    p=t;
-                    t=n;
-                    n=t[classNameProp];
-                    t[providerProp]=p;
-
-                }else if(p!==undefined){
-                    t[providerProp]=p;
-                }
-                container.set(n,t);
-            };
-            ///**** end internal ******************************************************
-
-            if(typeof $name!=='string'){
-                (name) ? _mapType(name,type,$name) : _mapType(type,$name);
-                return;
             }
-
-            if(!mapType_(type,$name,name)){
-                var timeoutId=setInterval(function(){
-                    if(mapType_(type,$name,name)){
-                        clearInterval(timeoutId);
+            if(length===3){
+                if(typeof $type !=='string'){
+                    type[providerProp]=$type;
+                    this.registerType(name,type);
+                }else{
+                    $type=this.getType($type,false);
+                    if($type){
+                        type[providerProp]=$type;
+                        this.registerType(name,type);
                     }else{
-                        count++;
-                        if(count > MAP_TYPE_MAX_COUNT){
-                            clearInterval(timeoutId);
-                        }
+                        this.getTypeAsync($type,false,function(dataType){
+                            if(dataType){
+                                type[providerProp]=dataType;
+                                self.registerType(name,type);
+                            } else console.log('warning: container.mapType could not locate type: ' + $type);
+                        });
                     }
-                },MAP_TYPE_INTERVAL)
+                }
             }
         }
 
